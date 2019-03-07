f0c2a259db080b00a5b4f764490c2871aea83e3f
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2019-03-07 16:03:04 +0100

    TexteAide

---
+++ TexteAide/cmake-build-debug/CMakeFiles/TexteAide.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TexteAide.dir/main.c.obj"
+  "TexteAide.pdb"
+  "TexteAide.exe"
+  "TexteAide.exe.manifest"
+  "libTexteAide.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TexteAide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
