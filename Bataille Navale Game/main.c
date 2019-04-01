edf44241f7920f33c53a57ab31f7caf32b0bdb86
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2019-04-01 09:36:21 +0200

    Sprint 4

--- Bataille Navale Game/main.c
+++ Bataille Navale Game/main.c
@@ -1,14 +1,27 @@
 //Title: Bataille Navale
 //Author: LPO
-//Date: 15.03.2019
+//Date: 01.04.2019
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <unistd.h>
 
 #pragma execution_character_set("UTF-8")
+#define SIZE 9
+#define STLC 218 // ┌, Single Top Left Corner
+#define STRC 191 // ┐, Single Top Right Corner
+#define SBLC 192 // └, Single Bottom Left Corner
+#define SBRC 217 // ┘, Single Bottom Right Corner
+#define SVSB 179 // │, Single Vertical Simple Border
+#define SVRB 180 // ┤, Single Vertical Right Border
+#define SVLB 195 // ├, Single Vertical Left Border
+#define SHSB 196 // ─, Single Horizontal Simple Border
+#define SHBB 193 // ┴, Single Horizontal Bottom Border
+#define SHTB 194 // ┬, Single Horizontal Top Border
+#define SC   197 // ┼, Single Center
 
-int Grille1[9][9] =
+
+int Grille1[SIZE][SIZE] =
         {
                 0, 4, 4, 4, 4, 0, 0, 0, 0,
                 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -21,7 +34,6 @@ int Grille1[9][9] =
                 1, 0, 0, 0, 0, 0, 2, 2, 0
         };
 int Coule[5] = {0, 0, 0, 0, 0};
-
 void coule(int x, int y)
 {
     //Fonction Coule
@@ -43,55 +55,74 @@ void coule(int x, int y)
     }
 }
 
-void Grille() {
-    int x;
-    int y;
-
-    printf("  X  1   2   3   4   5   6   7   8   9");
-    for (x = 0; x < 9; x++) {
-        printf("\n");
-
-        if (x == 0) {
-            printf("   ┌───┬───┬───┬───┬───┬───┬───┬───┬───┐\n  ");
-        }
-        if (x != 0) {
-            printf("   ├───┼───┼───┼───┼───┼───┼───┼───┼───┤\n  ");
-        }
-
-
-        printf("%d", x + 1);
-        for (y = 0; y < 9; y++) {
-
-            coule(x,y);
-
-            if (Grille1[x][y] == 0 || Grille1[x][y] == 1 || Grille1[x][y] == 2 || Grille1[x][y] == 3 ||
-                Grille1[x][y] == 4) {
-                printf("│   ");
-
-            } else if (Grille1[x][y] == -1) {
-                printf("│ ~ ");
-            } else if (Grille1[x][y] == 11 || Grille1[x][y] == 22 ||Grille1[x][y] == 23||Grille1[x][y] == 24) {
-                printf("│ X ");
-            } else if (Grille1[x][y] == 12 || Grille1[x][y] == 13 || Grille1[x][y] == 14) {
-                printf("│ x ");
-            }
-            if (y == 8) {
-                printf("│");
-            }
-        }
-
-        if (x == 8) {
-            printf("\n   └───┴───┴───┴───┴───┴───┴───┴───┴───┘");
-        }
+void topborder(int width) {
+    printf("     ");
+    printf("\n");
+    printf("   %c", STLC);       // ┌
+    for (int row = 1; row < width; row++) {
+        printf("%c%c%c%c", SHSB, SHSB, SHSB, SHTB);     // ┬
     }
+    printf("%c%c%c%c\n", SHSB, SHSB, SHSB, STRC);       // ┐
+}
+void verticalbars(int width, int row) {
+    char TabCase = 'x';
+    printf("%2d ", row + 1);    // Numéro de ligne
+    for (int colonne = 0; colonne < width; colonne++) {
+        TabCase = ' ';
+        coule(row,colonne);
+        if(Grille1[row][colonne] < 0){    // À l'eau
+            TabCase = '~';
+        }
+        if(Grille1[row][colonne] > 10){   // Touché
+            TabCase = 'x';
+        }
+        if(Grille1[row][colonne] > 20){   // Coulé
+            TabCase = '/';
+        }
+        printf("%c %c ", SVSB, TabCase);     // │ + Modèle
+    }
+    printf("%c", SVSB);
 }
 
+void horizonbars(int width) {
+    printf("\n   %c", SVLB);     // ├
+    for (int row = 1; row < width; row++) {
+        printf("%c%c%c%c", SHSB, SHSB, SHSB, SC);   // ┼
+    }
+    printf("%c%c%c%c\n", SHSB, SHSB, SHSB, SVRB);   // ┤
+}
+
+void bottombars(int width) {
+    printf("\n   %c", SBLC);     // └
+    for (int row = 1; row < width; row++) {
+        printf("%c%c%c%c", SHSB, SHSB, SHSB, SHBB);     // ┴
+    }
+    printf("%c%c%c%c", SHSB, SHSB, SHSB, SBRC);     // ┘
+}
+
+
+
+
+int grille(void) {
+    SetConsoleOutputCP(65001);      // Pour les caractères accentués
+    SetConsoleOutputCP(437);       // Pour les caractères semi-graphiques
+
+    printf("  X  1   2   3   4   5   6   7   8   9");
+    topborder(SIZE);        // Ligne du haut
+    for (int row = 0; row < SIZE; row++) {
+        if (row > 0) {
+            horizonbars(SIZE);      // Ligne du milieu
+        }
+        verticalbars(SIZE, row);    // Ligne de barres verticales
+    }
+    bottombars(SIZE);       // Ligne du bas
+}
 void Game() {
     int cvert;
     int chorz;
     int gameover = 0;
     do {
-        Grille();
+        grille();
         printf("\nIntroduisez la coordonée horizontal: ");
         scanf("%d", &cvert);
         printf("\nIntroduisez la coordonée vertical: ");
@@ -102,7 +133,10 @@ void Game() {
         if (o == 0) {
             Grille1[chorz - 1][cvert - 1] = -1;
 
-        } else if (o == 1 || o == 2 || o == 3 || o == 4) {
+        }else if(o == 1){
+            Coule[Grille1[chorz - 1][cvert - 1]] += 1;
+            Grille1[chorz - 1][cvert - 1] += 20;
+        }else if (o == 2 || o == 3 || o == 4) {
             Coule[Grille1[chorz - 1][cvert - 1]] += 1;
             Grille1[chorz - 1][cvert - 1] += 10;
 
@@ -211,7 +245,7 @@ int main() {
                         choix = +100;
                         break;
                     } else if (choix == 1) {
-                        Grille();
+                        grille();
                     } else {
                         printf("Ce n'est pas un choix !\n");
                     }
