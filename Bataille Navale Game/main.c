e1daf61bfc2a2be7177e44b133c1564754ea0e06
Luis Pedro Pinheiro <47849666+luispedropinheiro@users.noreply.github.com>
2019-04-04 16:15:59 +0200

    Sprint 5

--- Bataille Navale Game/main.c
+++ Bataille Navale Game/main.c
@@ -19,20 +19,67 @@
 #define SHBB 193 // ┴, Single Horizontal Bottom Border
 #define SHTB 194 // ┬, Single Horizontal Top Border
 #define SC   197 // ┼, Single Center
+#define nbgrilles 3
 
+void Bateau()
+{
+    //Boat
+    printf("                         \t  )___(\n"
+           "                           _______/__/_\n"
+           "                  ___     /===========|   ___\n"
+           " ____       __   [\\\\\\]___/____________|__[///]   __\n"
+           " \\   \\_____[\\\\]__/___________________________\\__[//]___\n"
+           "  \\40A                                                 |\n"
+           "   \\                                                  /\n"
+           "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-int Grille1[SIZE][SIZE] =
+}
+int Grille[SIZE][SIZE] = {};
+void chosegri() {
+
+    int choosegrille = 1000;
+    do
+    {
+        printf("\n");
+        for (int i = 0; i < nbgrilles; i++) {
+            printf("\tGrille %d",i+1);
+        }
+
+        printf("\n\nChossissez une grille: ");
+        scanf("%d", &choosegrille);
+
+        if (choosegrille<1 && choosegrille>nbgrilles)
         {
-                0, 4, 4, 4, 4, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 3, 0, 0, 0, 0,
-                0, 0, 0, 0, 3, 0, 0, 0, 0,
-                0, 0, 0, 0, 3, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0,
-                1, 0, 0, 0, 0, 0, 2, 2, 0
-        };
+            printf("\nCette grille n'est pas disponnible ou n'existe pas !");
+            choosegrille+=1000;
+        }
+    }while(choosegrille<1 && choosegrille>nbgrilles);
+
+    FILE *fichier = NULL;
+    switch (choosegrille)
+    {
+        case 1: fichier =fopen("Grille1.txt","r");
+            break;
+
+        case 2: fichier =fopen("Grille2.txt","r");
+            break;
+
+        case 3: fichier=fopen("Grille3.txt","r");
+
+
+    }
+    for (int x = 0; x < SIZE;x++)
+    {
+        for (int y = 0; y <SIZE ;y++)
+        {
+            char c = fgetc(fichier);
+            Grille[x][y]= c - '0';
+        }
+    }
+    fclose(fichier);
+
+
+}
 int Coule[5] = {0, 0, 0, 0, 0};
 void coule(int x, int y)
 {
@@ -45,9 +92,9 @@ void coule(int x, int y)
             {
                 for(int u =0; u < 9; u++)
                 {
-                    if(Grille1[x][y] == 10+i)
+                    if(Grille[x][y] == 10+i)
                     {
-                        Grille1[x][y] += 10;
+                        Grille[x][y] += 10;
                     }
                 }
             }
@@ -70,13 +117,13 @@ void verticalbars(int width, int row) {
     for (int colonne = 0; colonne < width; colonne++) {
         TabCase = ' ';
         coule(row,colonne);
-        if(Grille1[row][colonne] < 0){    // À l'eau
+        if(Grille[row][colonne] < 0){    // À l'eau
             TabCase = '~';
         }
-        if(Grille1[row][colonne] > 10){   // Touché
+        if(Grille[row][colonne] > 10){   // Touché
             TabCase = 'x';
         }
-        if(Grille1[row][colonne] > 20){   // Coulé
+        if(Grille[row][colonne] > 20){   // Coulé
             TabCase = '/';
         }
         printf("%c %c ", SVSB, TabCase);     // │ + Modèle
@@ -104,7 +151,6 @@ void bottombars(int width) {
 
 
 int grille(void) {
-    SetConsoleOutputCP(65001);      // Pour les caractères accentués
     SetConsoleOutputCP(437);       // Pour les caractères semi-graphiques
 
     printf("  X  1   2   3   4   5   6   7   8   9");
@@ -116,29 +162,32 @@ int grille(void) {
         verticalbars(SIZE, row);    // Ligne de barres verticales
     }
     bottombars(SIZE);       // Ligne du bas
+    SetConsoleOutputCP(65001);
 }
 void Game() {
     int cvert;
     int chorz;
     int gameover = 0;
     do {
+        Bateau();
+        printf("\n\n");
         grille();
         printf("\nIntroduisez la coordonée horizontal: ");
         scanf("%d", &cvert);
         printf("\nIntroduisez la coordonée vertical: ");
         scanf("%d", &chorz);
 
-        int o = Grille1[chorz - 1][cvert - 1];
+        int o = Grille[chorz - 1][cvert - 1];
 
         if (o == 0) {
-            Grille1[chorz - 1][cvert - 1] = -1;
+            Grille[chorz - 1][cvert - 1] = -1;
 
         }else if(o == 1){
-            Coule[Grille1[chorz - 1][cvert - 1]] += 1;
-            Grille1[chorz - 1][cvert - 1] += 20;
+            Coule[Grille[chorz - 1][cvert - 1]] += 1;
+            Grille[chorz - 1][cvert - 1] += 20;
         }else if (o == 2 || o == 3 || o == 4) {
-            Coule[Grille1[chorz - 1][cvert - 1]] += 1;
-            Grille1[chorz - 1][cvert - 1] += 10;
+            Coule[Grille[chorz - 1][cvert - 1]] += 1;
+            Grille[chorz - 1][cvert - 1] += 10;
 
         } else if (o > 10 || o == -1)
         {
@@ -166,21 +215,40 @@ void Game() {
 
     } while (gameover == 0);
 }
+void Jouer(int choix)
+{
+    system("cls");
+    Bateau();
+    printf("\n\n_____Jouer_____\n");
+    printf("\nBievenue sur le mode jeu\n");
+    printf("\nGrilles:");
+    chosegri();
+    Game();
+
+}
+void Instructions()
+{
+    system("cls");
+    printf("\n\n_____Instructions_____\n");
+    printf("\nPour démarrer une partie:");
+    printf("\nSur le Menu Principal: Entrez le chiffre 1 pour aller dans le menu jouer et ensuite il suffit d'entrer le chiffre de la grille que vous souhaitez");
+    printf("\n\nPour jouer une partie:");
+    printf("\nIl faut d'abord entrer la coordonnée horizontale de la case ou vous voulez tirer et ensuite la coordonée vertical. Il faut couler les 4 bateaux pour gagner. À la fin de la partie il suffit d'appuyer sur une touche pour revenir au Menu Principal.\n\n");
+    system("pause");
+}
+void Parametres()
+{
+    system("cls");
+    Bateau();
+    printf("\n\n_____Paramètres_____\n");
+    system("pause");
+}
 
 int main() {
     SetConsoleOutputCP(65001);
 
     int choix;
-
-    //Boat
-    printf("                         \t  )___(\n"
-           "                           _______/__/_\n"
-           "                  ___     /===========|   ___\n"
-           " ____       __   [\\\\\\]___/____________|__[///]   __\n"
-           " \\   \\_____[\\\\]__/___________________________\\__[//]___\n"
-           "  \\40A                                                 |\n"
-           "   \\                                                  /\n"
-           "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    Bateau();
 
     //Menu Principal
     do {
@@ -194,7 +262,7 @@ int main() {
         printf("\nIntroduisez un chiffre de 0 a 3: ");
         scanf("%d", &choix);
         if (choix < 0 || choix > 3) {
-            printf("Ceci n'est pas un choix !");
+            printf("\nCeci n'est pas un choix !");
         }
         switch (choix) {
             case 0:
@@ -202,58 +270,19 @@ int main() {
                 return 0;
 
             case 1:
-                printf("\n_____Jouer_____\n");
-                printf("\nBievenue sur le mode jeu");
-                do {
-                    printf("\nIntroduire 0 pour revenir en arriere ou 1 pour lancer une partie: \n");
-                    scanf("%d", &choix);
-                    if (choix == 0) {
-                        choix = +100;
-                        break;
-                    } else if (choix == 1) {
-                        Game();
-                    } else {
-                        printf("Ce n'est pas un choix !\n");
-                    }
-                } while (choix != 0 && choix != 1);
+                Jouer(choix);
                 break;
 
             case 2:
-                printf("\n\n_____Instructions_____\n");
-                printf("\nLes règles s'affichent...\n");
-                do {
-                    printf("\n0. Revenir en arriere\n");
-                    printf("\nIntroduire votre choix: ");
-                    scanf("%d", &choix);
-                    if (choix == 0) {
-                        choix = +100;
-                        break;
-                    } else {
-                        printf("Ce n'est pas un choix !\n");
-                    }
-                } while (choix != 0);
+                Instructions();
                 break;
 
             case 3:
-                printf("\n\n_____Paramètres_____\n");
-                do {
-                    printf("\n1. Modifier la grille");
-                    printf("\n0. Revenir en arriere");
-                    printf("\n\nIntroduire votre choix: ");
-                    scanf("%d", &choix);
-                    if (choix == 0) {
-                        choix = +100;
-                        break;
-                    } else if (choix == 1) {
-                        grille();
-                    } else {
-                        printf("Ce n'est pas un choix !\n");
-                    }
-                } while (choix != 0);
+                Parametres();
                 break;
         }
 
-    } while (choix < 0 || choix > 3);
+    } while (choix > 0 || choix < 3);
 
     return 0;
 }
